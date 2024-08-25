683e51ec752f40379b0653afe272961be542c5ed
Min Khant Thu <dsalpha3367@gmail.com>
2024-08-25 22:06:51 +0630

    [Add] Result Pattern

---
+++ DotNet8.EmailVerification.Utils/Result.cs
@@ -0,0 +1,79 @@
+﻿using DotNet8.EmailVerification.Utils.Enums;
+
+namespace DotNet8.EmailVerification.Utils
+{
+    public class Result<T>
+    {
+        public T Data { get; set; }
+        public EnumStatusCode StatusCode { get; set; }
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public static Result<T> Success(
+            string message = "Success",
+            EnumStatusCode statusCode = EnumStatusCode.Accepted)
+        {
+            return new Result<T>
+            {
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = true
+            };
+        }
+
+        public static Result<T> Success(
+            T Data,
+            string message = "Success",
+            EnumStatusCode statusCode = EnumStatusCode.Accepted)
+        {
+            return new Result<T>
+            {
+                Data = Data,
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = true
+            };
+        }
+
+        public static  Result<T> SaveSuccess(
+            string message="Saving Successful.",
+            EnumStatusCode statusCode=EnumStatusCode.Success)
+        {
+            return Result<T>.Success(message, statusCode);
+        }
+
+        public static Result<T> UpdateSuccess(
+            string message="Updating Successful.",
+            EnumStatusCode statusCode = EnumStatusCode.Success)
+        {
+            return Result<T>.Success(message,statusCode);
+        }
+
+        public static Result<T> Failure(
+            string message="Fail",
+            EnumStatusCode statusCode=EnumStatusCode.BadRequest)
+        {
+            return new Result<T>
+            {
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccess = false
+            };
+        }
+
+        public static Result<T> Failure(Exception ex)
+        {
+            return Result<T>.Failure(ex.ToString(),EnumStatusCode.InternalServerError);
+        }
+
+        public static Result<T> NotFound(string message="No data found")
+        {
+            return Result<T>.Failure(message, EnumStatusCode.NotFound);
+        }
+
+        public static Result<T> Duplicate(string message="Duplicate Data.")
+        {
+            return Result<T>.Failure(message, EnumStatusCode.Conflict);
+        }
+    }
+}
