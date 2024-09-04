d024ac5a09562ec4c8f3a068e7a703ad5b475d09
Min Khant Thu <dsalpha3367@gmail.com>
2024-09-04 15:12:32 +0630

    Add Json Convert Method

--- DotNet8.EmailVerification.Shared/DevCode.cs
+++ DotNet8.EmailVerification.Shared/DevCode.cs
@@ -1,7 +1,8 @@
-﻿namespace DotNet8.EmailVerification.Shared
-{
-    public static class DevCode
-    {
+﻿namespace DotNet8.EmailVerification.Shared;
 
-    }
+public static class DevCode
+{
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+
+    public static T? ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr);
 }
